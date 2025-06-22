c696475710f8a627a8ac25fdbfbaa931c2e30ae7
Henrique114 <h.ferreira114@gmail.com>
2025-06-22 01:46:06 -0300

    Criação da model EquipamentoEletronicoModel e do repository InMemoryRepository

---
+++ TestInvent/Models/EquipamentoEletronicoModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TestInvent.Models.Interfaces;
+
+namespace TestInvent.Models
+{
+    public class EquipamentoEletronicoModel : IEntity
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string nome { get; set; }
+        [Required]
+        public string tipo { get; set; }
+        [Required]
+        public int qntidadeEmEstoque { get; set; }
+        [Required]
+        public DateTime dataDeInclusao { get; set; }
+        public bool temEmEstoque { get; } = false;
+    }
+}
---
+++ TestInvent/Models/InMemoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using TestInvent.Models.Interfaces;
+
+
+namespace TestInvent.Models
+{
+    public class InMemoryRepository<T> : IRepository<T> where T : class, IEntity
+    {
+        private readonly List<T> _items = new();
+
+        public IEnumerable<T> ListAllEvents() => _items;
+
+        public T GetById(Guid id) => _items.SingleOrDefault(x => x.Id == id);
+
+        public void AddEvent(T entity)
+        {
+            entity.Id = Guid.NewGuid();
+            _items.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            Delete(entity.Id);
+            _items.Add(entity);
+        }
+
+        public void Delete(Guid id)
+        {
+            var existing = GetById(id);
+            if (existing != null) _items.Remove(existing);
+        }
+    } 
+   
+}
