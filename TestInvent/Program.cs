4c753781a8992003aeba38cc62f6b80f9a1ecd42
Henrique114 <h.ferreira114@gmail.com>
2025-06-24 15:50:06 -0300

    Refatoração da Program e Criação da MongoDbContex

--- TestInvent/Program.cs
+++ TestInvent/Program.cs
@@ -1,6 +1,7 @@
 
+using TestInvent.Data;
 using TestInvent.Models;
-using TestInvent.Models.Interfaces;
+using TestInvent.Repositories;
 
 namespace TestInvent
 {
@@ -10,8 +11,23 @@ namespace TestInvent
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Obtém as variáveis de ambiente diretamente do sistema
+            var mongoConnectionString = Environment.GetEnvironmentVariable("MongoSettings__ConnectionString");
+            var databaseName = Environment.GetEnvironmentVariable("MongoSettings__DatabaseName");
+
+            if (string.IsNullOrWhiteSpace(mongoConnectionString) || string.IsNullOrWhiteSpace(databaseName))
+            {
+                throw new Exception("As variáveis de ambiente MongoSettings__ConnectionString e MongoSettings__DatabaseName precisam estar definidas.");
+            }
+
+            // Registra o contexto Mongo
+            builder.Services.AddSingleton(sp => new MongoDbContext(mongoConnectionString, databaseName));
+
+            // Registra o repositório genérico
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(MongoDbRepository<>));
+
             // Add services to the container.
-            builder.Services.AddSingleton<IRepository<EquipamentoEletronicoModel>, InMemoryRepository<EquipamentoEletronicoModel>>();
+           
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
