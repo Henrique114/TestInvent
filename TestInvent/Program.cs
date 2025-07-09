09ef012a8620822d827a69d1b6100a94b1800b82
Henrique114 <h.ferreira114@gmail.com>
2025-07-09 02:22:59 -0300

    criação da class InMemoryRepository

--- TestInvent/Program.cs
+++ TestInvent/Program.cs
@@ -12,14 +12,6 @@ namespace TestInvent
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            //// Obtém as variáveis de ambiente diretamente do sistema
-            //var mongoConnectionString = Environment.GetEnvironmentVariable("MongoSettings__ConnectionString");
-            //var databaseName = Environment.GetEnvironmentVariable("MongoSettings__DatabaseName");
-
-            //if (string.IsNullOrWhiteSpace(mongoConnectionString) || string.IsNullOrWhiteSpace(databaseName))
-            //{
-            //    throw new Exception("As variáveis de ambiente MongoSettings__ConnectionString e MongoSettings__DatabaseName precisam estar definidas.");
-            //}
 
             // Registra o contexto Mongo
             builder.Services.AddSingleton(typeof(IMongoDbContext), typeof(MongoDbContext));
---
+++ TestInvent/Repositories/InMemoryRepository.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace TestInvent.Repositories
+{
+    public class InMemoryRepository<T> : IRepository<T> where T : class 
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    {
+    }
+}
