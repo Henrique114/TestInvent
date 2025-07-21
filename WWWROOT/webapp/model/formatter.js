f415e0dfa9e709610424910afe855d1d6f8be1a2
Henrique114 <h.ferreira114@gmail.com>
2025-07-21 17:52:58 -0300

    Criação do modelo da tela de listagem

---
+++ WWWROOT/webapp/model/formatter.js
@@ -0,0 +1,19 @@
+sap.ui.define([], () => {
+	"use strict";
+
+	return {
+		statusText(sStatus) {
+			const oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			switch (sStatus) {
+				case "A":
+					return oResourceBundle.getText("");
+				case "B":
+					return oResourceBundle.getText("");
+				case "C":
+					return oResourceBundle.getText("");
+				default:
+					return sStatus;
+			}
+		}
+	};
+});
