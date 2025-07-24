692db500b7b0638b8d35a66cf6635a593a73177c
Henrique114 <h.ferreira114@gmail.com>
2025-07-24 16:04:50 -0300

    Configuração de rotas e do launch; obs: mesmo com as rotas não esta acessando onInit da controller, em loop

--- TestInvent/wwwroot/controller/ListaEquipamentos.controller.js
+++ TestInvent/wwwroot/controller/ListaEquipamentos.controller.js
@@ -1,28 +1,45 @@
 sap.ui.define([
 	
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], (fetch, Controller) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast",
+    "sap/ui/core/UIComponent"
+], (fetch, Controller, JSONModel, MessageToast, UIComponent) => {
 	"use strict";
 
-	return Controller.extend("ui5.testinvent.controller.ListaEquipamentos", {
-		onInit: function() {
-			 // Exemplo de requisição GET
+    const MODELO_EQUIPAMENTO = "equipamentos";
+
+    return Controller.extend("ui5.testinvent.controller.ListaEquipamentos", {
+        
+        onInit: function () {
+            debugger
+            this.roteador = UIComponent.getRouterFor(this);
+            this.roteador.getRoute("teste").attachPatternMatched(this._aoAcessarEditar, this);
+        },
+
+        _aoAcessarEditar: function () {
+            debugger
             fetch("https://localhost:7104/EquipamentoEletronico")
-                .then(function(response) {
+                .then(function (response) {
+                    debugger
                     return response.json(); // ou response.text(), response.blob(), etc.
                 })
-                .then(function(data) {
+                .then(function (data) {
                     // Manipula os dados recebidos
-                    console.log(data);
-					MessageToast.show(data);
+                    const dados = data.json();
+                    console.log(dados);
+                    MessageToast.show(dados);
+
+                    const oModel = new sap.ui.model.json.JSONModel(dados);
+                    this.getView().setModel(oModel, "equipamentos");
                 })
-                .catch(function(error) {
+                .catch(function (error) {
                     // Trata erros da requisição
                     console.error("Erro na requisição:", error);
                 });
-			
-
-		}
-	});
+        }
+        
+    }); 
+       
+	
 });
--- TestInvent/wwwroot/controller/Painel.controller.js
+++ TestInvent/wwwroot/controller/Painel.controller.js
@@ -2,34 +2,64 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
    "sap/m/MessageToast",
-], (Controller, MessageToast) => {
+   "sap/ui/core/UIComponent"
+], (Controller, MessageToast, UIComponent) => {
    "use strict";
 
    return Controller.extend("ui5.testinvent.controller.Painel", {
-      onShowHello() {
-         // // read msg from i18n model
-         // const oBundle = this.getView().getModel("i18n").getResourceBundle();
-         // const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-         // const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+      // onShowHello() {
+      //    debugger
+      //    // // read msg from i18n model
+      //    // const oBundle = this.getView().getModel("i18n").getResourceBundle();
+      //    // const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+      //    // const sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
-         // // show message
+      //    // // show message
         
-           fetch("/EquipamentoEletronico")
-                 .then(function(response) {
-                     return response.json(); // ou response.text(), response.blob(), etc.
-                 })
-                 .then(function(data) {
-                     // Manipula os dados recebidos
-                     console.log(data);
-			 		MessageToast.show(data);
-                 })
-                 .catch(function(error) {
-                     // Trata erros da requisição
-                     console.error("Erro na requisição:", error);
-                 });
+      //      fetch("/EquipamentoEletronico")
+      //            .then(function(response) {
+      //                return response.json(); // ou response.text(), response.blob(), etc.
+      //            })
+      //            .then(function(data) {
+      //                // Manipula os dados recebidos
+      //                console.log(data);
+		// 	 		MessageToast.show(data);
+      //            })
+      //            .catch(function(error) {
+      //                // Trata erros da requisição
+      //                console.error("Erro na requisição:", error);
+      //            });
 			
 
          
-      }
+      // }
+
+      onInit: function () {
+            debugger
+            this.roteador = UIComponent.getRouterFor(this);
+            this.roteador.getRoute("teste").attachPatternMatched(this._aoAcessarEditar, this);
+        },
+
+        _aoAcessarEditar: function () {
+            debugger
+            fetch("https://localhost:7104/EquipamentoEletronico")
+                .then(function (response) {
+                    debugger
+                    return response.json(); // ou response.text(), response.blob(), etc.
+                })
+                .then(function (data) {
+                    // Manipula os dados recebidos
+                    const dados = data.json();
+                    console.log(dados);
+                    MessageToast.show(dados);
+
+                    const oModel = new sap.ui.model.json.JSONModel(dados);
+                    this.getView().setModel(oModel, "equipamentos");
+                })
+                .catch(function (error) {
+                    // Trata erros da requisição
+                    console.error("Erro na requisição:", error);
+                });
+        }
    });
 });
