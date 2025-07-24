67d6e2e4aea4fdae2f10c17bc6e3a4582dc7d8c1
Henrique114 <h.ferreira114@gmail.com>
2025-07-24 16:13:54 -0300

    implementando a listagem em uma so view para teste (Painel.view)

--- TestInvent/wwwroot/controller/Painel.controller.js
+++ TestInvent/wwwroot/controller/Painel.controller.js
@@ -5,7 +5,37 @@ sap.ui.define([
 ], (Controller, MessageToast) => {
    "use strict";
 
-   return Controller.extend("ui5.testinvent.controller.Painel", {
+    return Controller.extend("ui5.testinvent.controller.Painel", {
+
+
+        onInit: function () {
+            debugger
+            this.roteador = UIComponent.getRouterFor(this);
+            this.roteador.getRoute("teste").attachPatternMatched(this._aoAcessarEditar, this);
+        },
+
+        _aoAcessarEditar: function () {
+            debugger
+            fetch("https://localhost:7104/EquipamentoEletronico")
+                .then(function (response) {
+                    debugger
+                    return response.json(); // ou response.text(), response.blob(), etc.
+                })
+                .then(function (data) {
+                    // Manipula os dados recebidos
+                    const dados = data.json();
+                    console.log(dados);
+                    MessageToast.show(dados);
+
+                    const oModel = new sap.ui.model.json.JSONModel(dados);
+                    this.getView().setModel(oModel, "equipamentos");
+                })
+                .catch(function (error) {
+                    // Trata erros da requisição
+                    console.error("Erro na requisição:", error);
+                });
+        },
+
       onShowHello() {
          // // read msg from i18n model
          // const oBundle = this.getView().getModel("i18n").getResourceBundle();
