60c9d98a5d18a4787cdeda9bb88089632cf2aaf5
Henrique114 <h.ferreira114@gmail.com>
2025-07-24 23:29:20 -0300

    alterado obtencao do certificado pela variavel de ambiente #time 10m

--- TestInvent/Data/RavenDB/RavenDbContext.cs
+++ TestInvent/Data/RavenDB/RavenDbContext.cs
@@ -3,38 +3,37 @@ using Raven.Client.Documents;
 
 public class RavenDbContext
 {
-    private static Lazy<IDocumentStore> store = new Lazy<IDocumentStore>(CreateStore);
-
-    public static IDocumentStore Store => store.Value;
+    private static Lazy<IDocumentStore> _store = new Lazy<IDocumentStore>(CreateStore);
+    public static IDocumentStore Store => _store.Value;
 
     private static IDocumentStore CreateStore()
     {
-        //var caminhoCert = config["CertificadoBase64"];
-        //var password = config[""];
+        const string nomeVariavelCertificado = "CertificadoBase64";
+        var caminhoCertificadoRaven = Environment.GetEnvironmentVariable(nomeVariavelCertificado);
 
-        //if (!File.Exists(caminhoCert))
-        //    throw new FileNotFoundException("Arquivo de certificado não encontrado.", caminhoCert);
+        if (!File.Exists(caminhoCertificadoRaven))
+            throw new FileNotFoundException("Arquivo de certificado não encontrado.", caminhoCertificadoRaven);
 
-        //// Leitura e limpeza
-        //var base64Content = File.ReadAllText(caminhoCert)
-        //    .Replace("\r", "")
-        //    .Replace("\n", "")
-        //    .Replace(" ", "")
-        //    .Trim();
+        // Leitura e limpeza
+        var base64Content = File.ReadAllText(caminhoCertificadoRaven)
+            .Replace("\r", "")
+            .Replace("\n", "")
+            .Replace(" ", "")
+            .Trim();
 
-        //var certBytes = Convert.FromBase64String(base64Content);
+        var certBytes = Convert.FromBase64String(base64Content);
 
-        //var cert = new X509Certificate2(certBytes, password,
-        //    X509KeyStorageFlags.MachineKeySet |
-        //    X509KeyStorageFlags.PersistKeySet |
-        //    X509KeyStorageFlags.Exportable);
+        var cert = new X509Certificate2(certBytes, string.Empty,
+            X509KeyStorageFlags.MachineKeySet |
+            X509KeyStorageFlags.PersistKeySet |
+            X509KeyStorageFlags.Exportable);
 
         IDocumentStore store = new DocumentStore()
         {
             Urls = new[] { Environment.GetEnvironmentVariable("RavenDbUrl")
         },
-            Database = Environment.GetEnvironmentVariable("RavenDbSettings"),
-            //Certificate = cert
+            Database = "Inventario",
+            Certificate = cert
         }.Initialize();
         return store;
     }
