2bf1b56574cc5efb7867daf5643590fb0cbbdbfd
Henrique114 <h.ferreira114@gmail.com>
2025-08-05 11:51:16 -0300

    Ajuste no nome das variaveis e metodos; criação do metodo criarDialogo

--- TestInvent/wwwroot/webapp/controller/ListagemEquipamentos.controller.js
+++ TestInvent/wwwroot/webapp/controller/ListagemEquipamentos.controller.js
@@ -12,13 +12,12 @@ sap.ui.define([
     return Controller.extend("ui5.testinvent.controller.ListagemEquipamentos", {
         onInit: function () {
             this._oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
-            const ROTA = this.getOwnerComponent().getRouter();
-            ROTA.getRoute(ROTA_LISTAGEM).attachPatternMatched(this._aoAcessarListar, this);
+            const rota = this.getOwnerComponent().getRouter();
+            rota.getRoute(ROTA_LISTAGEM).attachPatternMatched(this._aoAcessarListar, this);
         
         },
 
         _aoAcessarListar: function () {
-            
             this._obterDadosEquipamentos();
         },
 
@@ -31,13 +30,11 @@ sap.ui.define([
                 .then(dados => {
                     dados.forEach(element => {
                         element.dataDeInclusao = new Date(element.dataDeInclusao);
-                        element.descricao = "aqui vai uma descrição do equipamento"; // Simulação de descrição
                     });
 
-                    
-                    const oModel = new JSONModel(dados);
-                    this.getView().setModel(oModel, MODELO_EQUIPAMENTOS);
-                })
+                    const model = new JSONModel(dados);
+                    this.getView().setModel(model, MODELO_EQUIPAMENTOS);
+            })
                 
         },
 
@@ -52,48 +49,38 @@ sap.ui.define([
             const equipamentoSelecionado = event.getSource().getBindingContext(MODELO_EQUIPAMENTOS).getObject();
             const oModelEquipamento = new JSONModel(equipamentoSelecionado);
             this.getView().setModel(oModelEquipamento, "modeloDialogo");
-            this.onOpenDialog();
-
-            
+            this.aoAbrirFragmentoDialogo();
         },
 
-        onOpenDialog: function() {
-            var oView = this.getView();
-            console.log(oView);
+        criarDialogo: function(view) {
+            return Fragment.load({
+                id: view.getId(),
+                name: "ui5.testinvent.view.DetalhesEquipamento",
+                controller: this
+            }).then((dialog) => {
+                dialog.setModel(); // você pode passar o modelo necessário como argumento, se desejar
+                this.getOwnerComponent().getModel("i18n").getResourceBundle();   
+                view.addDependent(dialog);
+                this.oDialog = dialog;
+                return dialog;
+            });
+        },
 
-            // Verifica se o fragmento já foi carregado
+        aoAbrirFragmentoDialogo: function() {
+           var view = this.getView();
+
             if (!this.byId("idDialog")) {
-                Fragment.load({
-                    id: oView.getId(),
-                    name: "ui5.testinvent.view.DetalhesEquipamento",
-                    controller: this
-                }).then((oDialog)=> {
-                    oDialog.setModel();
-                    this.getResourceBundle();   
-                    oView.addDependent(oDialog);
-                    this.oDialog = oDialog; // Armazena a referência do diálogo
-                    oDialog.open();
+                this.criarDialogo(view)
+                .then((dialog) => {
+                    dialog.open();
                 });
             } else {
                 this.byId("idDialog").open();
             }
         },
 
-         onDialogClose: function(oEvent) {
-        this.oDialog.destroy(); 
-        this.oDialog = null;
-        },
-
-         aoPrecionarFechar: function(oEvent) {
+         aoPrecionarFechar: function(evento) {
         this.oDialog.close();
-        },
-    
-        getResourceBundle: function() {
-            const nome = "i18n";
-            return this
-                .getOwnerComponent()
-                .getModel(nome)
-                .getResourceBundle();
         }
    });
 });
