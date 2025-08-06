e188f8c968aae65e136e5598a4224f6d8bf9839f
Henrique114 <h.ferreira114@gmail.com>
2025-08-06 09:14:26 -0300

    implementação da função PrimeiraLetraEmCaixaAlta

--- TestInvent/Extensions/ExtensaoDeStrings.cs
+++ TestInvent/Extensions/ExtensaoDeStrings.cs
@@ -4,9 +4,20 @@ namespace TestInvent.Extensions
 {
     public static class ExtensaoDeStrings
     {
-        public static string DecodificarURL(this string url) 
+        public static string DecodificarURL(this string url)
         {
             return HttpUtility.UrlDecode(url);
         }
-    }
+
+
+        public static string PrimeiraLetraEmCaixaAlta(this string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+
+            return char.ToUpper(text[0]) + text.Substring(1);
+        }
+    }  
 }
