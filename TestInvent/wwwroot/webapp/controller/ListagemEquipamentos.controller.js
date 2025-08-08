83dd3fdf3d82bd24e62d5664410a9187fcf351ab
Henrique114 <h.ferreira114@gmail.com>
2025-08-08 14:10:00 -0300

    Declarado constandes de escopo no  wwwroot/webapp/controller/ListagemEquipamentos.controller.js; ajuste na identação wwwroot/webapp/i18n/i18n.properties; ajuste nomes de variaveis e metodos; exclusão de arquivos e properties não utilizadas.

--- TestInvent/wwwroot/webapp/controller/ListagemEquipamentos.controller.js
+++ TestInvent/wwwroot/webapp/controller/ListagemEquipamentos.controller.js
@@ -1,17 +1,24 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/core/Fragment",
+    "sap/ui/core/Fragment"
 ],(Controller, JSONModel, Fragment) => {
     "use strict";
 
     const ENDPOINT_BASE = "/EquipamentoEletronico";
     const ROTA_LISTAGEM = "ListagemEquipamentos";
     const MODELO_EQUIPAMENTOS = "equipamentos";
+    const MODELO_TRADUCAO = "i18n";
+    const ID_TELA_DETALHES = "idDialog";  
+    const ID_TELA_NOVO_EQUIPAMENTO = "idCadastroEAlterar";
+    const NOME_FRAGMENT_DETALHES = "ui5.testinvent.view.DetalhesEquipamento";
+    const NOME_FRAGMENT_NOVO_EQUIPAMENTO = "ui5.testinvent.view.AdicionarEAtualizarEquipamento";
+    const ITEM_SELECIONADO_LISTA = "modeloDialogo";
+    const NOVO_EQUIPAMENTO = "modeloEquipamento";
 
     return Controller.extend("ui5.testinvent.controller.ListagemEquipamentos", {
         onInit: function () {
-            this._oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            this._oResourceBundle = this.getOwnerComponent().getModel(MODELO_TRADUCAO).getResourceBundle();
             const rota = this.getOwnerComponent().getRouter();
             rota.getRoute(ROTA_LISTAGEM).attachPatternMatched(this._aoAcessarListar, this);
         
@@ -30,8 +37,10 @@ sap.ui.define([
                 .then(dados => {
                     dados.forEach(element => {
                         element.dataDeInclusao = new Date(element.dataDeInclusao);
-                        element.tipo = this._mapearTipoDoEquipamneto(element.tipo);
+                        //element.tipo = this._mapearTipoDoEquipamneto(element.tipo);
                     });
+
+                    console.log(dados);
 
                     const model = new JSONModel(dados);
                     this.getView().setModel(model, MODELO_EQUIPAMENTOS);
@@ -49,20 +58,20 @@ sap.ui.define([
             
             const equipamentoSelecionado = event.getSource().getBindingContext(MODELO_EQUIPAMENTOS).getObject();
             const oModelEquipamento = new JSONModel(equipamentoSelecionado);
-            this.getView().setModel(oModelEquipamento, "modeloDialogo");
+            this.getView().setModel(oModelEquipamento, ITEM_SELECIONADO_LISTA);
             this.AoAbrirTelaDeDetalhes();
         },
 
         AoAbrirTelaDeDetalhes: function() {
            var view = this.getView();
 
-            if (!this.byId("idDialog")) {
+            if (!this.byId(ID_TELA_DETALHES)) {
                 this.criarTelaDeDetalhes(view)
                 .then((dialog) => {
                     dialog.open();
                 });
             } else {
-                this.byId("idDialog").open();
+                this.byId(ID_TELA_DETALHES).open();
             }
         },
 
@@ -70,11 +79,11 @@ sap.ui.define([
           
             return Fragment.load({
                 id: view.getId(),
-                name: "ui5.testinvent.view.DetalhesEquipamento",
+                name: NOME_FRAGMENT_DETALHES,
                 controller: this
             }).then((dialog) => {
                 dialog.setModel(); 
-                this.getOwnerComponent().getModel("i18n").getResourceBundle();   
+                this.getOwnerComponent().getModel(MODELO_TRADUCAO).getResourceBundle();   
                 view.addDependent(dialog);
                 this.oDialog = dialog;
                 return dialog;
@@ -90,41 +99,41 @@ sap.ui.define([
          AoAbrirTelaDeNovoEquipamento: function() {
            var view = this.getView();
 
-            if (!this.byId("idCadastroEAlterar")) {
+            if (!this.byId(ID_TELA_NOVO_EQUIPAMENTO)) {
                 this.criarTelaDeNovoEquipamento(view)
                 .then((dialog) => {
                     dialog.open();
                 });
             } else {
-                this.byId("idCadastroEAlterar").open();
+                this.byId(ID_TELA_NOVO_EQUIPAMENTO).open();
             }
         },
 
-         criarTelaDeNovoEquipamento: function(view) {
+        criarTelaDeNovoEquipamento: function(view) {
             
             return Fragment.load({
                 id: view.getId(),
-                name: "ui5.testinvent.view.AdicionarEAtualizarEquipamento",
+                name: NOME_FRAGMENT_NOVO_EQUIPAMENTO,
                 controller: this
             }).then((dialog) => {
 
-                dialog.setModel(new JSONModel({}), "modeloEquipamento");
+                dialog.setModel(new JSONModel({}), NOVO_EQUIPAMENTO);
                 this._carregarTiposEquipamento();
                 dialog.setModel(this.getView().getModel("modeloTipoEquipamento")); 
-                this.getOwnerComponent().getModel("i18n").getResourceBundle();   
+                this.getOwnerComponent().getModel(MODELO_TRADUCAO).getResourceBundle();   
                 view.addDependent(dialog);
                 this.oDialog = dialog;
                 return dialog;
             });
         },
 
-        aoPrecionarFechar: function(evento) {
+        aoPressionarFechar: function(evento) {
             this.oDialog.close();
         },
 
-        aoPrecionarSalvar: function(evento) {
-            const dialog = this.byId("idCadastroEAlterar");
-            const dados = dialog.getModel("modeloEquipamento").getData();
+        aoPressionarSalvar: function(evento) {
+            const dialog = this.byId(ID_TELA_NOVO_EQUIPAMENTO);
+            const dados = dialog.getModel(NOVO_EQUIPAMENTO).getData();
             const dadosTipo = dialog.getModel("modeloTipoEquipamento").getData();
 
             const objeto = {
