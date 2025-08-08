a07c2b3801726df1b8d90ba7b2d9a4bdb032ba32
Henrique114 <h.ferreira114@gmail.com>
2025-08-08 18:01:06 -0300

    - Criação da classe ExtencoesEnum e implementação dos metodos PegarDescrição, CriarListaDeTiposParaSelectDoFormulario;

---
+++ TestInvent/Extensions/ExtencoesEnum.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.Reflection;
+using TestInvent.Models;
+
+namespace TestInvent.Extensions
+{
+    public static class ExtencoesEnum
+    {
+        public static string PegarDescrição(this Enum valor)
+        {
+            var campo = valor.GetType().GetField(valor.ToString());
+            var atributo = (DescriptionAttribute)Attribute.GetCustomAttribute(campo, typeof(DescriptionAttribute));
+            return atributo == null ? valor.ToString() : atributo.Description;
+        }
+
+        public static List<Enumerador> CriarListaDeTiposParaSelectDoFormulario<TEnum>() where TEnum : Enum
+        {
+            var listaDeItens = new List<Enumerador>();
+            foreach (TEnum item in Enum.GetValues(typeof(TEnum)))
+            {
+                listaDeItens.Add(new Enumerador
+                {
+                    Chave = ((int)(object)item).ToString(),
+                    Descricao = item.PegarDescrição()
+                });
+            }
+            return listaDeItens;
+        }
+
+    }
+}
