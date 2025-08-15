998ef2d919621ed700f9106cc4590d2f7516bbb5
Henrique114 <h.ferreira114@gmail.com>
2025-08-15 15:23:56 -0300

    Refatoração na classe Enumerador, Classe e Atributo chave tipo TEnum; refatoração no metodo CriarListaDeTiposParaSelectDoFormulario, tipo metodo TEnum -> T, retorno List<TEnum> -> List<Enumerador<T>>; refatoração na conversao do  valor que sera atribuido ao campo chave.

--- TestInvent/Models/Enumerador.cs
+++ TestInvent/Models/Enumerador.cs
@@ -1,8 +1,8 @@
 ﻿namespace TestInvent.Models
 {
-    public class Enumerador
+    public class Enumerador <TEnum>
     {
-        public int? Chave { get; set; }
+        public TEnum Chave { get; set; }
         public string? Descricao { get; set; }
     }
 }
