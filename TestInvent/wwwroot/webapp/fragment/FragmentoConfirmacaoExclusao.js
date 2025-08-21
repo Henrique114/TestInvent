f1ca22c73106d148b77a1c812e3c65f47367e785
Henrique114 <h.ferreira114@gmail.com>
2025-08-21 17:13:43 -0300

    Implemntaçao do fragment/FragmentoConfirmacaoExclusao -> criarDialogDeConfirmação.

---
+++ TestInvent/wwwroot/webapp/fragment/FragmentoConfirmacaoExclusao.js
@@ -0,0 +1,49 @@
+ sap.ui.define([
+     "sap/m/Dialog",
+     "sap/m/Button",
+     "sap/m/MessageToast",
+     "sap/m/Bar",
+     "sap/m/Title",
+     "sap/ui/core/library",
+     "sap/m/Text"
+     ], function(Dialog, Button, MessageToast, Bar, Title, coreLibrary, Text) {
+     return {
+         criarDialogDeConfirmação: function(controller, idEquipamento) {
+             let confirmacaoDeletar = new Dialog({
+                     content: [
+                         new Text({
+                             text: "Tem certeza que deseja apagar este equipamento?"
+                         })
+                     ],
+                     state: 'Warning',
+                     beginButton: new Button({
+                         press: function () {
+                             this.getParent().close();
+                         },
+                         text: "Close"
+                     }),
+                     endButton: new Button({
+                         press: () => {
+                             controller.aoPressinarConfirmar(idEquipamento);
+                             MessageToast.show("Equipamento deletado!");
+                             confirmacaoDeletar.close();
+                         },
+                         text: "Confirmar"
+                     }),
+                     customHeader: new Bar({
+                         contentMiddle: [
+                             new Title({
+                                 text: "Confirmar exclusão do equipamento",
+                                 level: coreLibrary.TitleLevel.H1
+                             })
+                         ]
+                     }),
+                     contentHeight: "10%",
+                     contentWidth: "20%",
+                     verticalScrolling: false
+                 });
+ 
+             return  confirmacaoDeletar;
+         }
+     }
+ });
