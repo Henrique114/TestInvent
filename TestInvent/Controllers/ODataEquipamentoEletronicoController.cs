d36dd2f0131cc882366559edf733383fc2c5a1f1
Henrique114 <h.ferreira114@gmail.com>
2025-08-22 18:52:57 -0300

    tent 1

---
+++ TestInvent/Controllers/ODataEquipamentoEletronicoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using TestInvent.Data;
+
+namespace TestInvent.Controllers
+{
+    public class ODataEquipamentoEletronicoController
+    {
+        [Route("odata/[controller]")]
+        public class EquipamentoEletronicoController : ODataController
+        {
+            [EnableQuery]
+            [HttpGet]
+            public IActionResult Get()
+            {
+                return Ok(EquipamentoData.GetEquipamentos());
+            }
+        }
+    }
+}
---
+++ TestInvent/Data/EquipamentoData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TestInvent.Models;
+
+namespace TestInvent.Data
+{
+    public class EquipamentoData
+    {
+        private static List<EquipamentoEletronicoModel> _equipamentos = new List<EquipamentoEletronicoModel>
+        {
+            new() { Id = "1", Nome = "Dell", Tipo = (EnumeracaoTipos?)1, QuantidadeEmEstoque = 10, DataDeInclusao = new DateTimeOffset(), Descricao= "uma breve descrição" },
+            new() { Id = "2", Nome = "Dell", Tipo = (EnumeracaoTipos?)1, QuantidadeEmEstoque = 10, DataDeInclusao = new DateTimeOffset(), Descricao= "uma breve descrição" }
+        };
+
+        public static IQueryable<EquipamentoEletronicoModel> GetEquipamentos() => _equipamentos.AsQueryable();
+    }
+}
